7905b706812b2790a4ca71505c000108021bf595
Justin Lohner <lohner4@gmail.com>
2020-07-13 17:43:24 -0600

    wrote queue class using linked list as storage structure

--- queue/queue.py
+++ queue/queue.py
@@ -13,36 +13,43 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
 		What would that look like? How many Stacks would you need? Try it!
 """
+
+from singly_linked_list import LinkedList
+
 # --- array ---
+# class Queue:
+# 	def __init__(self):
+# 		self.size = 0
+# 		self.storage = []
+#
+# 	def __len__(self):
+# 		return self.size
+#
+# 	def enqueue(self, value):
+# 		self.storage.append(value)
+# 		self.size += 1
+#
+# 	def dequeue(self):
+# 		if not self.size:
+# 			return None
+# 		self.size -= 1
+# 		return self.storage.pop(0)
+
+# --- linked list ---
 class Queue:
 	def __init__(self):
 		self.size = 0
-		self.storage = []
+		self.storage = LinkedList()
 
 	def __len__(self):
 		return self.size
 
 	def enqueue(self, value):
-		self.storage.append(value)
+		self.storage.add_to_tail(value)
 		self.size += 1
 
 	def dequeue(self):
 		if not self.size:
 			return None
 		self.size -= 1
-		return self.storage.pop(0)
-
-# --- linked list ---
-# class Queue:
-# 	def __init__(self):
-# 		self.size = 0
-# 		# self.storage = ?
-#
-# 	def __len__(self):
-# 		pass
-#
-# 	def enqueue(self, value):
-# 		pass
-#
-# 	def dequeue(self):
-# 		pass
+		return self.storage.remove_head()
