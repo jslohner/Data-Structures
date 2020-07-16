8861ced6bd36362a16efb612ee4fda70513e5b86
Justin Lohner <lohner4@gmail.com>
2020-07-16 12:09:45 -0600

    Added pseudocode for binary search tree file

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,46 +17,67 @@ class BSTNode:
 
 	# Insert the given value into the tree
 	def insert(self, value):
+		# if value < current node value
 		if value < self.value:
+			# check if left attr node is none and if it is
+			# create a new node with the passed in value
 			if self.left is None:
 				self.left = BSTNode(value)
+			# otherwise call insert function on left attr node
 			else:
 				self.left.insert(value)
+		# check if value >= current node value
 		else:
+			# check if right attr node is none and if it is
+			# create a new node with the passed in value
 			if self.right is None:
 				self.right = BSTNode(value)
+			# otherwise call insert function on right attr node
 			else:
 				self.right.insert(value)
 
 	# Return True if the tree contains the value
 	# False if it does not
 	def contains(self, target):
+		# if target value equals current node value return true
 		if target == self.value:
 			return True
+		# check if target < current node value
 		elif target < self.value:
+			# if left attr node is none return false
 			if self.left is None:
 				return False
+			# otherwise return contains function call on left attr node
 			else:
 				return self.left.contains(target)
+		# check if target >= current node value
 		else:
+			# if right attr node is none return false
 			if self.right is None:
 				return False
+			# otherwise return contains function call on right attr node
 			else:
 				return self.right.contains(target)
 
 	# Return the maximum value found in the tree
 	def get_max(self):
+		# if right side of tree is none - meaning there are no
+		# greater values than current node value - return current node value
 		if self.right is None:
 			return self.value
+		# otherwise return get_max function call on right attr node
 		else:
 			return self.right.get_max()
 
 	# Call the function `fn` on the value of each node
 	def for_each(self, fn):
+		# call function on current node value
 		fn(self.value)
+		# if left attr is not none call for_each function on left attr node
 		if self.left is not None:
 			self.left.for_each(fn)
 
+		# if right attr is not none call for_each function on right attr node
 		if self.right is not None:
 			self.right.for_each(fn)
 
